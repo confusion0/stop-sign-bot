bbe3ab992cb4ba3af157f3e1b99d156f1c36c1e1
confusion <74419921+confusion0@users.noreply.github.com>
2021-07-29 16:23:01 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = async () => {
-  await mongoose.connect('mongodb+srv://xsadme:lmfao888@cluster0.upphd.mongodb.net/MySecondDatabase?retryWrites=true&w=majority', {
+  await mongoose.connect('mongoURI', {
   	keepAlive: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
